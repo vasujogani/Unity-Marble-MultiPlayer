ad27a7bec55fb2d0e4fd8ab7d431227ef89e47a9
nathanhoang5 <nathanhoang5@gmail.com>
2016-09-15 13:03:25 -0500

    Add files via upload

---
+++ Rotate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	int rot = -10;
+	bool stopLine = true;
+
+	void Update () 
+	{
+		
+		float x = gameObject.transform.rotation.x; 
+		Debug.Log (x);
+		if (stopLine) {
+			if (x < -0.2) {
+				rot = 10;
+				stopLine = false;
+			}
+		} else {
+			if (x > 0.2) {
+				rot = -10;
+				stopLine = true;
+			}
+		}
+		transform.Rotate (new Vector3 (rot, 0, 0) * Time.deltaTime);
+	}
+}
